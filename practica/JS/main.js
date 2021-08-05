5d7541debfb54124e8de981836c215b0cc169e57
GastonSalomon <gastonmsalomon@gmail.com>
2021-08-05 11:19:19 -0300

    segundo comit

---
+++ practica/JS/main.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Ingresa tu nombre");
+console.log(nombre);
+
+let apellido = prompt("Ingresa tu apellido");
+console.log(apellido);
+
+let edad = prompt("Ingresa tu edad");
+
+alert("Vos te llamas" + nombre + " " + apellido + " tenes " + edad + " años ");
