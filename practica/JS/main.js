e9f5c37402c64ce63e8fe8b55290100e93172762
GastonSalomon <gastonmsalomon@gmail.com>
2021-08-17 16:15:47 -0300

    tercer commit - entrega 17/08

--- practica/JS/main.js
+++ practica/JS/main.js
@@ -13,6 +13,21 @@ if (edad >= 18) {
   alert("Sos muy chico para entrar.");
 }
 
+let pregunta = parseInt(prompt("¿del 0 al 10 , cuanto te gusto?"));
+while (pregunta != "ESC") {
+  switch (pregunta) {
+    case "0":
+      alert("¿Tan mal salio?");
+      break;
+      case "5":
+      alert("¿?Que podemos mejorar");
+      break;
+  }
+    case "10":
+      alert("¡Muchas gracias!");
+      break;
+  }
+}
 /*let edadFutura = prompt("¿Elegí un numero del 0 al 10?");
 let sumaEdades = parseInt(edad) + parseInt(edadFutura);
 console.log(sumaEdades);*/
